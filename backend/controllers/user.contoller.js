5c36bee292f8cec395899e776b74fd77a07444ff
Jawad <jawadal829@gmail.com>
2025-02-27 19:35:00 +0500

    controllers added

--- backend/controllers/user.contoller.js
+++ backend/controllers/user.contoller.js
@@ -32,28 +32,34 @@ export const new_user = async (req, res) => {
 };
 
 export const login_user = async (req, res) => {
-  const { password, email } = req.body;
-  const findUser = await User.findOne({ email });
-  if (!findUser) {
-    return res.status(300).json({
-      message: "Account not found...",
+  try {
+    const { password, email } = req.body;
+    const findUser = await User.findOne({ email });
+    if (!findUser) {
+      return res.status(300).json({
+        message: "Account not found...",
+      });
+    }
+    const comparedPassword = await comparePassword(password, findUser.password);
+    if (!comparedPassword) {
+      return res.status(301).json({
+        message: "Invalid Password...",
+      });
+    }
+    const token = await signJsonwebtoken(findUser);
+    if (!token) {
+      return res.status(302).json({
+        message: "Error while generating token...",
+      });
+    }
+    return res.status(200).json({
+      message: "Logged in...",
+      user: findUser,
+      token,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Server error...",
     });
   }
-  const comparedPassword = await comparePassword(password, findUser.password);
-  if (!comparedPassword) {
-    return res.status(301).json({
-      message: "Invalid Password...",
-    });
-  }
-  const token = await signJsonwebtoken(findUser);
-  if (!token) {
-    return res.status(302).json({
-      message: "Error while generating token...",
-    });
-  }
-  return res.status(200).json({
-    message: "Logged in...",
-    user: findUser,
-    token,
-  });
 };
